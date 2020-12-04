18c489c83c92dfef24f7dacc2775ad5825ca878d
Pablo Moreno <pablo.moreno.olalla@gmail.com>
2020-12-04 16:10:52 +0100

    [AOC4] Day 4 code. More regexes! Also generic interfaces and all that shit I like.

---
+++ src/main/java/com/nbrevu/advent2020/Advent41.java
@@ -0,0 +1,27 @@
+package com.nbrevu.advent2020;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.io.Resources;
+
+public class Advent41 {
+	private final static String IN_FILE="Advent41.txt";
+	private final static Set<String> REQUIRED_FIELDS=ImmutableSet.of("byr","iyr","eyr","hgt","hcl","ecl","pid");
+	
+	public static void main(String[] args) throws IOException	{
+		int counter=0;
+		URL file=Resources.getResource(IN_FILE);
+		Set<String> currentSet=new HashSet<>();
+		for (String line:Resources.readLines(file,Charsets.UTF_8)) if (line.isBlank())	{
+			if (currentSet.containsAll(REQUIRED_FIELDS)) ++counter;
+			currentSet.clear();
+		}	else for (String split:line.split(" ")) currentSet.add(split.split(":")[0]);
+		if (currentSet.containsAll(REQUIRED_FIELDS)) ++counter;
+		System.out.println(counter);
+	}
+}
