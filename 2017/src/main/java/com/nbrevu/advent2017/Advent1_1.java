f6148478bda5db56a65f7312ede20afa45a7af76
Pablo Moreno <pablo.moreno.olalla@gmail.com>
2020-12-08 19:47:49 +0100

    [AOC-17] 2017's structure and first problem.

---
+++ 2017/src/main/java/com/nbrevu/advent2017/Advent1_1.java
@@ -0,0 +1,24 @@
+package com.nbrevu.advent2017;
+
+import java.io.IOException;
+import java.net.URL;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+public class Advent1_1 {
+	private final static String IN_FILE="Advent1.txt";
+	
+	public static void main(String[] args) throws IOException	{
+		URL file=Resources.getResource(IN_FILE);
+		String content=Resources.readLines(file,Charsets.UTF_8).get(0);
+		char[] chars=content.toCharArray();
+		int result=0;
+		for (int i=0;i<chars.length;++i)	{
+			char c=chars[i];
+			char next=chars[(i+1)%chars.length];
+			if (c==next) result+=c-'0';
+		}
+		System.out.println(result);
+	}
+}
