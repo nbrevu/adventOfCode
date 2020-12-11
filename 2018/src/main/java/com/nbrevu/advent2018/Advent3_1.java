6278af655dcfcbeb780eb90a7fa2220d85194c39
Pablo Moreno <pablo.moreno.olalla@gmail.com>
2020-12-11 01:35:30 +0100

    [AOC-18] 2018-3. I've always liked this way to find whether two rectangles intersect.

---
+++ 2018/src/main/java/com/nbrevu/advent2018/Advent3_1.java
@@ -0,0 +1,44 @@
+package com.nbrevu.advent2018;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+public class Advent3_1 {
+	private final static String IN_FILE="Advent3.txt";
+	private final static int SIZE=1000;
+	
+	private final static Pattern LINE_PATTERN=Pattern.compile("^#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)$");
+	
+	private static class Fabric	{
+		private final int[][] claims;
+		public Fabric(int size)	{
+			claims=new int[size][size];
+		}
+		public void claim(int offsetX,int offsetY,int width,int height)	{
+			int finalX=offsetX+width;
+			int finalY=offsetY+height;
+			for (int i=offsetX;i<finalX;++i) for (int j=offsetY;j<finalY;++j) ++claims[i][j];
+		}
+		public int countMultipleClaims()	{
+			int result=0;
+			for (int i=0;i<claims.length;++i) for (int j=0;j<claims[i].length;++j) if (claims[i][j]>=2) ++result;
+			return result;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException	{
+		URL file=Resources.getResource(IN_FILE);
+		Fabric fabric=new Fabric(SIZE);
+		for (String line:Resources.readLines(file,Charsets.UTF_8))	{
+			Matcher matcher=LINE_PATTERN.matcher(line);
+			if (matcher.matches()) fabric.claim(Integer.parseInt(matcher.group(2)),Integer.parseInt(matcher.group(3)),Integer.parseInt(matcher.group(4)),Integer.parseInt(matcher.group(5)));
+		}
+		System.out.println(fabric.countMultipleClaims());
+	}
+
+}
---
+++ 2018/src/main/java/com/nbrevu/advent2018/Advent3_2.java
@@ -0,0 +1,61 @@
+package com.nbrevu.advent2018;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+public class Advent3_2 {
+	private final static String IN_FILE="Advent3.txt";
+	
+	private final static Pattern LINE_PATTERN=Pattern.compile("^#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)$");
+	
+	private static class Claim	{
+		public final int id;
+		public final int offsetX;
+		public final int offsetY;
+		public final int width;
+		public final int height;
+		public Claim(int id,int offsetX,int offsetY,int width,int height)	{
+			this.id=id;
+			this.offsetX=offsetX;
+			this.offsetY=offsetY;
+			this.width=width;
+			this.height=height;
+		}
+		public boolean overlaps(Claim other)	{
+			int minX=Math.max(offsetX,other.offsetX);
+			int maxX=Math.min(offsetX+width,other.offsetX+other.width);
+			if (maxX<=minX) return false;
+			int minY=Math.max(offsetY,other.offsetY);
+			int maxY=Math.min(offsetY+height,other.offsetY+other.height);
+			return minY<maxY;
+		}
+	}
+	
+	private static boolean[] findOverlaps(List<Claim> claims)	{
+		boolean[] result=new boolean[claims.size()];
+		for (int i=0;i<claims.size();++i) for (int j=i+1;j<claims.size();++j) if (claims.get(i).overlaps(claims.get(j)))	{
+			result[i]=true;
+			result[j]=true;
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) throws IOException	{
+		URL file=Resources.getResource(IN_FILE);
+		List<Claim> claims=new ArrayList<>();
+		for (String line:Resources.readLines(file,Charsets.UTF_8))	{
+			Matcher matcher=LINE_PATTERN.matcher(line);
+			if (matcher.matches()) claims.add(new Claim(Integer.parseInt(matcher.group(1)),Integer.parseInt(matcher.group(2)),Integer.parseInt(matcher.group(3)),Integer.parseInt(matcher.group(4)),Integer.parseInt(matcher.group(5))));
+		}
+		boolean[] overlaps=findOverlaps(claims);
+		for (int i=0;i<overlaps.length;++i) if (!overlaps[i]) System.out.println(claims.get(i).id);
+	}
+
+}
