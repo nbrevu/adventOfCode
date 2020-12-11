bad5cbcd79668969cbc32aa30c9b97b77f47e85e
Pablo Moreno <pablo.moreno.olalla@gmail.com>
2020-12-11 01:07:27 +0100

    [AOC-18] 2018 with structure and first problem. Also a reduced gitignore.

---
+++ 2018/src/main/java/com/nbrevu/advent2018/Advent1_1.java
@@ -0,0 +1,20 @@
+package com.nbrevu.advent2018;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+public class Advent1_1 {
+	private final static String IN_FILE="Advent1.txt";
+	
+	public static void main(String[] args) throws IOException	{
+		URL file=Resources.getResource(IN_FILE);
+		List<String> lines=Resources.readLines(file,Charsets.UTF_8);
+		int result=lines.stream().mapToInt(Integer::parseInt).sum();
+		System.out.println(result);
+	}
+
+}
---
+++ 2018/src/main/java/com/nbrevu/advent2018/Advent1_2.java
@@ -0,0 +1,30 @@
+package com.nbrevu.advent2018;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import com.koloboke.collect.set.IntSet;
+import com.koloboke.collect.set.hash.HashIntSets;
+
+public class Advent1_2 {
+	private final static String IN_FILE="Advent1.txt";
+	
+	public static void main(String[] args) throws IOException	{
+		URL file=Resources.getResource(IN_FILE);
+		List<String> lines=Resources.readLines(file,Charsets.UTF_8);
+		int[] values=lines.stream().mapToInt(Integer::parseInt).toArray();
+		IntSet partialSums=HashIntSets.newMutableSet();
+		int sum=0;
+		for (;;) for (int i:values)	{
+			sum+=i;
+			if (partialSums.contains(sum))	{
+				System.out.println(sum);
+				return;
+			}	else partialSums.add(sum);
+		}
+	}
+
+}
